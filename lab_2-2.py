97b6d0352b982c27b7bc5f894371827ab4f88941
Brian Connolly <p23bconnolly@fairfieldprep.org>
2022-09-15 07:56:29 -0700

    finished 2-1, 2-2, 2-3

---
+++ lab_2-2.py
@@ -0,0 +1,7 @@
+a = 3
+b = a 
+#the value of a is 3 due to me setting it as 3, and the value of b is 3 because i set it to equal a 
+a = 7
+print(a)
+print(b)
+#the value of a is 7 because i set it to now equal 7, while b is equal to 3 due to it still being on the first command setting it to 3
---
+++ lab_2-3.py
@@ -0,0 +1,12 @@
+(a) = 2
+print (type(a))
+a=float(a)
+# the class has changed into float
+print (type(a))
+a = str(a) 
+print(type(a))
+# the class has changed into str
+a = bool(a)
+print(a == 2)
+# it is fasle because the bool makes a either true or false, not a number
+ 
