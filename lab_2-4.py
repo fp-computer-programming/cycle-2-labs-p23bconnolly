fd65581d820ccb9cea7ff97b5147fd724ebf71a6
Brian Connolly <p23bconnolly@fairfieldprep.org>
2022-09-19 09:14:22 -0700

    competed 4 and 5

---
+++ lab_2-4.py
@@ -0,0 +1,23 @@
+a = 1
+b = 2
+c = 3
+x = 2
+y = 5
+
+r2 = (3+(4*x))/5
+print(r2)
+
+d2 = (10*(y-5)*(a+b+c)**2)/x
+print(d2)
+
+y1 = 4/x**2
+print(y1)
+
+c3PO = (9+x)/y
+print(c3PO)
+
+DV = 9 * (y1 + ((9+x)/y))
+print(DV)
+
+print(r2-d2+DV)
+
