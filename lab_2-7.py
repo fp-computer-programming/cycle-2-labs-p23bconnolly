263e0cae0c14dd58cb8e19639311aeb5594dcd4c
Brian Connolly <p23bconnolly@fairfieldprep.org>
2022-09-21 11:14:31 -0700

    completed labs 2.6 and 2.7

---
+++ lab_2-7.py
@@ -0,0 +1,48 @@
+a = 5
+b = 10
+c = 0 
+d = -4
+if a % 2 == 0:
+    print("a is an even number")
+else:
+    print("a is an odd number") 
+if b % 2 == 0:
+    print("b is an even number")
+else:
+    print("b is an odd number")
+if c % 2 == 0:
+    print("c is an even number")
+else:
+    print("c is an odd number")
+if d % 2 == 0:
+    print("d is an even number")
+else:
+    print("d is an odd number")
+
+if a < -5:
+    print("less than -5")
+elif a > 5:
+    print("greater than 5")
+else:
+    print("between range") 
+
+if b < -5:
+    print("less than -5")
+elif b >= 5:
+    print("greater than 5")
+else:
+    print("between range")
+
+if c < -5:
+    print("less than -5")
+elif c >= 5:
+    print("greater than 5")
+else:
+    print("between range")
+if d < -5:
+    print("less than -5")
+elif d >= 5:
+    print("greater than 5")
+else:
+    print("between range")
+    
