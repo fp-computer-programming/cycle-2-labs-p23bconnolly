5ccdf0b005e25566739f1bd809e05c8f99ad15c5
Brian Connolly <p23bconnolly@fairfieldprep.org>
2022-09-22 08:36:35 -0700

    completed labs 2-8 and 2-9.

---
+++ lab_2-8.py
@@ -0,0 +1,17 @@
+x = 11
+
+if x >= 15:
+    print("you got a gold medal")
+else:
+    if x >= 12:
+        print("you got a silver medal")
+    else:
+        if x >= 9:
+            print("you got a bronze medal") 
+        else: 
+            print("you get no medal") 
+
+
+
+                    
+
---
+++ lab_2-9.py
@@ -0,0 +1,10 @@
+x = 1
+
+if x >= 15:
+    print("you got a gold medal")
+elif x >= 12: 
+    print("you got a silver medal")
+elif x >= 9:    
+    print("you got a bronze medal") 
+else: 
+    print("you get no medal")
